1582771dd41ded25b7a2f09d556f6b9b821aabeb
Accarie <Accarie>
2024-12-15 19:20:47 +0200

    Initial commit

---
+++ accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+# accounts/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from .forms import AdminRegistrationForm, AdminLoginForm
+
+def register(request):
+    if request.method == 'POST':
+        form = AdminRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful. Please log in.')
+            return redirect('login')
+    else:
+        form = AdminRegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AdminLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.error(request, 'Invalid username or password')
+    else:
+        form = AdminLoginForm()
+    return render(request, 'login.html', {'form': form})
